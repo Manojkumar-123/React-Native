a5e05f52df814409a1d4263ac6b872d66126cd2a
Manojkumar K <Manojkumar.ka@avasoft.com>
2023-09-21 10:50:11 +0530

    sep 21 DELL

--- demo/App.js
+++ demo/App.js
@@ -1,15 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [currentGoal, setCurrentGoal] = useState("");
+  const [goalsList, setGoalsList] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setCurrentGoal(enteredText);
+  }
+
+  const addGoalHandler = () => {
+    setGoalsList((currentGoalsList) => [
+      ...currentGoalsList, 
+      {text: currentGoal, id: Math.random().toString},
+    ]);
+    setCurrentGoal("");
+  }
+
+  const clearGoalHandler = () => {
+    setGoalsList([]);
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder='Your Course Goal ..!' style={styles.textInput}></TextInput>
-        <Button title='Add Goal'/>
+        <TextInput style={styles.textInput} placeholder="Your course goal!" onChangeText={goalInputHandler} value={currentGoal} id='goalInput'/>
+        <Button title="Add Goal" onPress={addGoalHandler}/>
       </View>
-      <View style={styles.listContainer}>
-        <Text>List of Goals....</Text>
+      <View style={styles.goalsContainer}>
+        {goalsList.length == 0 ? <Text>No goals yet...</Text> : <FlatList data={goalsList} renderItem={itemData => {
+          return(
+            <View style={styles.goalText}>
+              <Text>{itemData.item.text}</Text>
+            </View>
+          );
+          }} 
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+          alwaysBounceVertical={false}/>
+        }
+      </View>
+      <View style={{marginBottom: 50}}>
+        <Button title='Clear Goals' onPress={clearGoalHandler}></Button>
       </View>
     </View>
   );
@@ -17,19 +51,36 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc'
   },
   textInput: {
     borderWidth: 1,
-    borderColor: '#000000',
-    width: '80%',
-    padding: 5,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8
   },
-  listContainer: {
-    marginTop: 10
+  goalsContainer: {
+    flex: 5
+  },
+  goalText: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 10,
+    backgroundColor: '#cccc',
+    height: 40,
+    justifyContent: 'center',
+    alignContent: 'center'
   }
 });
